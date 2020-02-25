814b171d76f18d33a3027b5be928935df4bb7651
Miguel Cristobal <mcristobal@localhost.localdomain>
2020-02-25 07:25:09 -0600

    7:20 AM

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ app.engine('ejs', engine); /*Con esto tenemos un nuevo motor de plantilla defini
 app.set('view engine', 'ejs'); /*Utilizando el motor de plantillas*/
 app.set('views', path.join(__dirname, 'views'));/*Metodo JOIN une directorios. Concatenamos __dirname con la carpeta views*/
 /*CONSTANTE __dirname Nos devuelve la ruta inicial, devuelve la direccion del archivo que lo ejecuta. Equivalente a PWD*/
+
 //middlewares
 app.use(morgan('dev')) /*Estamos utilizando morgan con su configuracion de development*/
 
-
+//routes
+app.use(require('./routes/index'))
 app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'));
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,10 @@
 const express = require('express');/*Lo vamos a requerir para usar un metodo de este Framework(router)*/
+const router = express.Router();
+const Instagram = require('node-instagram');/*Lo vamos a utilizar para hacer autenticacion de un usuario con Instagram dentro de nuestra aplicacion y tambien para poder pedir los datos*/
+
+router.get('/', (req, res) => {
+    res.render('index')
+})/*Cuando el usuario pida algo a la ruta inicial de la aplicacion...*/
+module.exports = router;/*Se exporta para indicar que se va a utilizar en la aplicacion principal*/
+
+// router.get('/login', )
